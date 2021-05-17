896ed220ca9bb48b4dd7caa44979facf089aad8d
THINKPAD\Mateo <mposada@lean-tech.io>
2021-05-17 18:41:52 -0500

    test

---
+++ src/routes/tasks.routes.ts
@@ -0,0 +1,18 @@
+import {Router} from 'express'
+import {getTasks} from '../controllers/task.controller'
+
+const router = Router()
+
+router.get('/tasks', getTasks)
+
+router.get('/tasks/count', (req, res) => res.send('Hello Wold!'))
+
+router.post('/tasks', (req, res) => res.send('Hello Wold!'))
+
+router.get('/tasks/:id', (req, res) => res.send('Hello Wold!'))
+
+router.delete('/tasks/:id', (req, res) => res.send('Hello Wold!'))
+
+router.put('/tasks/:id', (req, res) => res.send('Hello Wold!'))
+
+export default router
