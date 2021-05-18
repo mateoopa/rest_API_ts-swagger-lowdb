51694bdc3818a23e0682e6338a00c096f26fca67
THINKPAD\Mateo <mposada@lean-tech.io>
2021-05-17 21:25:25 -0500

    last fucntions and swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan';
 
+//Swagger
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
+import {options} from './swaggerOptions'
+
 import taskRoutes from './routes/tasks.routes'
 
 const app = express()
@@ -12,6 +17,9 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
+const specs = swaggerJsDoc(options)
+
 app.use(taskRoutes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 export default app
